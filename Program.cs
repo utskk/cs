98a93606c3a91f17bc563d756dfa3e0347b54d15
Utku Işık <97395397+utskk@users.noreply.github.com>
2022-10-25 16:25:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.logger = new FileLogger();
+            customerManager.Add();
+            
+            customerManager.Delete();
+
+           
+        }
+    }
+   class CustomerManager
+    {
+        public ILogger logger { get; set; }
+        public void Add()
+        {
+            logger.Log();
+            Console.WriteLine("Customer added !");
+        }
+        public void Delete()
+        {
+            logger.Delete();
+            Console.WriteLine("Customer Deleted.!");
+        }
+
+    }
+    interface ILogger
+    {
+        void Log();
+        void Delete();
+    }
+   class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file.!");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("..!");
+        }
+
+    }
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database.!");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("..!");
+        }
+
+    }
+
+}
