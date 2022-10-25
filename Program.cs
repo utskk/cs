f9867d3109f0665a0b91ef0087865290c93a16ea
Utku Işık <97395397+utskk@users.noreply.github.com>
2022-10-25 15:29:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Oracle();
+            Database database1 = new SqlServer();
+            database.Add();
+            database1.Add();
+            
+            database.Delete();
+            database1.Delete();
+
+
+        }
+          abstract class Database {
+        public void Add()
+            {
+                Console.WriteLine("Database added.");
+            }
+            abstract public void Delete();
+        }
+        class Oracle : Database
+        {
+            public override void Delete()
+            {
+                Console.WriteLine("Deleted by Oracle.");
+            }
+        }
+        class SqlServer : Database
+        {
+            public override void Delete()
+            {
+                Console.WriteLine("Deleted by Sql.");
+            }
+        }
+    }
+}
